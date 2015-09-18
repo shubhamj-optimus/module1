bdd35678bcc46ec921df7aba47b1d080139be725
shubham <shubham.jan@optimusinfo.com>
2015-09-18 15:49:38 +0530

    new commit

---
+++ src/firstjavaclass.java
@@ -0,0 +1,42 @@
+//this is a sample program
+public class firstjavaclass {
+
+	// function in which control comes for first time
+	
+	public static void main(String[] args) {
+	  
+		System.out.println("Learning selenium");
+		
+		//datatypes
+		 int i=100;
+		 System.out.println(i);
+		 i=1000;
+		 System.out.println(i);
+		 double d=12.12;
+		 System.out.println(d);
+		 double e=12;
+		 System.out.println(e);
+		 char f='a'; 
+		 System.out.println(f);
+		 
+		 System.out.println(24>5);
+		 int x=50;
+		 int y=400;
+		 if(x>y){
+			 System.out.println("x "+x);			 
+		 }
+		 else {
+			 System.out.println("y is greatest");
+		 }
+		 
+		 String g = "its a dummy";
+		 System.out.println(g);
+		 
+		 
+		 
+		 
+		 
+	}
+}
+
+ 
